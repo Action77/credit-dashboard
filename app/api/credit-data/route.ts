a17b72cf423dfe9ad9eef6d1f432bd2c7ccc30d2
Hussain <you@example.com>
2026-07-31 01:15:15 +0300

    Update

--- app/api/credit-data/route.ts
+++ app/api/credit-data/route.ts
@@ -8,11 +8,11 @@ const supabase = createClient(
 
 export async function GET() {
   const { data, error } = await supabase
-    .from("credit_data_full")
-    .select("*")
-    .order("created_at", {
-      ascending: false,
-    });
+  .from("credit_data_full")
+  .select("*")
+  .order("created_at", {
+    ascending: false,
+  });
 
   if (error) {
     return NextResponse.json({
@@ -48,24 +48,24 @@ export async function GET() {
   }
 
   const formattedData = data.map((row) => ({
-    "Van Code.": row.van_code,
-    "Employee Name.": row.employee_name,
-    "Employee ATS Code.": row.employee_ats_code,
-    "Customer Code": row.customer_code,
-    "Customer Name": row.customer_name,
-    "Central Invoice": row.central_invoice,
-    "Payment Term": row.payment_term,
-    "Invoice #": row.invoice,
-    "Trx Date": row.trx_date,
-    "Credit Invoice Amount":
-      row.credit_invoice_amount,
-    "Pending CIM": row.pending_cim,
-    "Credit_Days": row.credit_days,
-    "Total Rejected Count":
-      row.total_rejected_count,
-    Region: row.region,
-    City: row.city,
-  }));
+  "Van Code.": row.van_code,
+  "Employee Name.": row.employee_name,
+  "Employee ATS Code.": row.employee_ats_code,
+  "Customer Code": row.customer_code,
+  "Customer Name": row.customer_name,
+  "Central Invoice": row.central_invoice,
+  "Payment Term": row.payment_term,
+  "Invoice #": row.invoice,
+  "Trx Date": row.trx_date,
+  "Credit Invoice Amount": row.credit_invoice_amount,
+  "Pending CIM": row.pending_cim,
+  "Credit_Days": row.credit_days,
+  "Total Rejected Count": row.total_rejected_count,
+  "Status User Block": row.status_user_block,
+  "Invoice status (Due/ Overdue)": row.invoice_status,
+  Region: row.region,
+  City: row.city,
+}));
 
   return NextResponse.json({
     data: formattedData,
