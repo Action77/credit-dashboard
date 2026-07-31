9c2ab8095b37f12b823b98772a7472848cbf0875
Hussain <you@example.com>
2026-07-31 13:16:04 +0300

    add web-push types

--- app/api/send-push/route.ts
+++ app/api/send-push/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
-import webpush from "web-push";
+const webpush = require("web-push");
 
 webpush.setVapidDetails(
   process.env.VAPID_EMAIL!,
