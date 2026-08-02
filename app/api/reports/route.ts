7519a03fb4ef67e68fc8362398cdeab39fc2278a
Hussain <you@example.com>
2026-08-02 09:11:13 +0300

    add web-push types

--- app/api/reports/route.ts
+++ app/api/reports/route.ts
@@ -8,29 +8,32 @@ const supabase = createClient(
 
 export async function GET() {
 
-  const { data: creditData } =
+  const { data: creditData, error } =
     await supabase
       .from("credit_data_full")
       .select(`
-  region,
-  city,
-  van_code,
-  employee_name,
-  employee_ats_code,
-  customer_code,
-  customer_name,
-  central_invoice,
-  payment_term,
-  invoice,
-  trx_date,
-  credit_invoice_amount,
-  collect_amount,
-  pending_cim,
-  credit_days,
-  invoice_status,
-  status_user_block,
-  total_rejected_count
-`);
+        region,
+        city,
+        van_code,
+        employee_name,
+        employee_ats_code,
+        customer_code,
+        customer_name,
+        central_invoice,
+        payment_term,
+        invoice,
+        trx_date,
+        credit_invoice_amount,
+        collect_amount,
+        pending_cim,
+        credit_days,
+        invoice_status,
+        status_user_block,
+        total_rejected_count
+      `);
+
+  console.log("ERROR:", error);
+  console.log("ROWS:", creditData?.length);
 
   const { data: collections } =
     await supabase
