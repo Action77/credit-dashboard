dcba8db05a2415c17ccecc284a046ec924f3bffb
Action77 <120266162+Action77@users.noreply.github.com>
2026-08-05 09:11:58 +0300

    Hide notification bell on van page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -107,22 +107,23 @@ const rows: any[] =
               row["Contact"] || "",
           }));
 
-        await fetch("/api/users", {
-          method: "POST",
-          headers: {
-            "Content-Type":
-              "application/json",
-          },
-          body: JSON.stringify({
-            users: usersData,
-          }),
-        });
-
         const currentUser =
-          await localStorage.getItem(
-            "currentUser"
-          );
+  await localStorage.getItem(
+    "currentUser"
+  );
 
+await fetch("/api/users", {
+  method: "POST",
+  headers: {
+    "Content-Type":
+      "application/json",
+  },
+  body: JSON.stringify({
+    users: usersData,
+    uploadedBy: currentUser,
+  }),
+});
+   
         let fullName = "";
 
         if (currentUser) {
